899cb13adb6525cebff78dee336e8a1608ec0e4d
Nick <afagu.13@gmail.com>
2020-10-24 18:21:21 +0300

    ClassWork 24/10/2020

--- content/article.php
+++ content/article.php
@@ -13,17 +13,19 @@ if (!$_GET) {
     echo '<ul>';
     foreach ($form as $value) {
         ?>
-        <li><a href="/ithive.loc/index.php?showForm=<?=$value['id'] ?>">Ссылка на форму <?= $value['nameOfForm']?> </a></li>
+        <li><a href="/ithive.loc/index.php?showForm=<?= $value['id'] ?>">Ссылка на форму <?= $value['nameOfForm'] ?> </a></li>
         <?php
     }
     echo '</ul>';
 
-
-
-
-} else {echo 'crash';
-   // $qq = \vendor\classes\Form::getFromDB(2, 1);
-   // $qq->viewForm();
+    if (empty($_GET['showForm'])) {
+        $formOutput = \vendor\classes\Form::getSingleForm($_GET['showForm']);
+        $formOutput->viewForm();
+    }
+} else {
+    echo 'crash';
+    // $qq = \vendor\classes\Form::getFromDB(2, 1);
+    // $qq->viewForm();
 }
 
 //$formOutput =\vendor\classes\Form::getSingleForm(1);
