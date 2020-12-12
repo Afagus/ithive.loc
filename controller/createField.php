f2763b9e461e1d8b049a12672cf7ac46447ee1a2
Nick <afagu.13@gmail.com>
2020-12-12 11:16:45 +0200

    ClassWork 05/12/2020 test

--- controller/createField.php
+++ controller/createField.php
@@ -1,27 +1,30 @@
+<?php
 mydebugger($_POST);
 
-$stringSelect = explode(PHP_EOL, $_POST['infoForSelect']);
-$stringSelect = implode(',', $stringSelect);
+$stringSelect = explode( PHP_EOL,$_POST['infoForSelect']);
+$stringSelect = implode(',' , $stringSelect);
 
-preg_match_all("#([^,\s]+):([^,\s]+)#s", $stringSelect, $out);
-$outSelect = array_combine($out[1], $out[2]);
+preg_match_all("#([^,\s]+):([^,\s]+)#s",$stringSelect,$out);
+$outSelect = array_combine($out[1],$out[2]) ;
 $outSelect = json_encode($outSelect, JSON_FORCE_OBJECT);
+
+
 
 
 $database = \database\singleConnect::getInstance();
 $sql = "INSERT INTO table_form_building
 (form_ID, name, type_ID, placeholder, value, validation_ID, labelForLetter, options, title)
 VALUES (" .
-ROUTE[1] . ', ' .
-'\'' . $_POST['nameField'] . '\'' . ', ' .
-'\'' . $_POST['typeField'] . '\'' . ', ' .
-'\'' . $_POST['placeholderField'] . '\'' . ', ' .
-'\'\'' . ', ' .
-'\'' . $_POST['validationField'] . '\'' . ', ' .
-'\'' . $_POST['labelForLetterField'] . '\'' . ', ' .
-'\'' . $outSelect . '\'' . ', ' .
-'\'' . "NULL" . '\'' .
-")";
+            ROUTE[1] . ', '.
+            '\''. $_POST['nameField']. '\''.', ' .
+            '\''. $_POST['typeField']. '\''.', ' .
+            '\''. $_POST['placeholderField']. '\''.', ' .
+            '\'\''.', ' .
+            '\''. $_POST['validationField']. '\''.', ' .
+            '\''. $_POST['labelForLetterField']. '\''.', ' .
+            '\''. $outSelect . '\''.', ' .
+            '\''. "NULL". '\''.
+             ")";
 $sqlFields = $database->query($sql);
 header("HTTP/1.1. 301 Moved Permanently");
 $string = "Location: $_SERVER[HTTP_REFERER]";
