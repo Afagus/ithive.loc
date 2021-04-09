33cb76357fd2884f21ee16083b0cd9fc4a23f2bf
Nick <afagu.13@gmail.com>
2021-04-09 18:16:06 +0300

    ClassWork 09/04/2021

--- content/form.php
+++ content/form.php
@@ -1,6 +1,6 @@
 <form action="/ithive.loc/createForm" method="post">
     <input type="text" value = "" placeholder="Input name of Form" name="name">
-    <input type="submit" name="createForm" value="create" >
+    <input id="createFormBtn" type="submit" name="createForm" value="create" >
     <br>
     <br>
 </form>
---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*Create variable*/
+let createFormBtn = document.getElementById("createFormBtn");
+
+/**/
+
+createFormBtn.addEventListener("click", function () {
+    alert("hello");
+});
